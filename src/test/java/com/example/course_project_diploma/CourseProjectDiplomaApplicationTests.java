6b28b9aa93ecfbc2d75b27dadbac3fc03ca57655
Sasha Chementsova <chementsova2102@gmail.com>
2023-11-21 23:30:14 +0300

    entities add

---
+++ src/test/java/com/example/course_project_diploma/CourseProjectDiplomaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.course_project_diploma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseProjectDiplomaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
