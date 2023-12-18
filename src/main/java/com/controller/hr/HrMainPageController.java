849b8198d9d0c2c87f6cc8624bee4f4ae8f1dffc
Sasha Chementsova <chementsova2102@gmail.com>
2023-12-18 08:44:01 +0300

    commit

--- src/main/java/com/controller/hr/HrMainPageController.java
+++ src/main/java/com/controller/hr/HrMainPageController.java
@@ -1,5 +1,7 @@
 package com.controller.hr;
 
+import com.service.CandidateService;
+import com.service.EmployeeService;
 import com.service.HrService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,14 +10,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HrMainPageController {
-    private final HrService hrService;
+    private final  EmployeeService employeeService;
+    private  final CandidateService candidateService;
     @Autowired
-    public HrMainPageController(HrService hrService) {
-        this.hrService = hrService;
+    public HrMainPageController(EmployeeService employeeService, CandidateService candidateService) {
+        this.employeeService = employeeService;
+        this.candidateService = candidateService;
     }
 
     @GetMapping("/hr/hrPage")
     public String hrHome(Model model){
+        model.addAttribute("employee",employeeService.getEmployees().size());
+        model.addAttribute("candidate",candidateService.getCandidates().size());
         return "hr/hr.html";
     }
 
